907509b24af89f18d4dbfcd8062d69b7396a3fa1
Shuaiyin <shuaiyin.top@foxmail.com>
2021-06-14 21:24:36 +0800

    开发环境

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ function component(parms) {
     const element = document.createElement('div');
     const btn = document.createElement('button');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Helloaaa', 'webpack'], ' ');
 
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
 
     element.appendChild(btn);
-    
+
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '管理输出'
+            title: '开发环境'
         })
     ],
     output: {
         filename: '[name].boundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        publicPath: '/'
     },
 
 };
